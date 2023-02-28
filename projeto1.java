1f2122646ab19ffd3ce510ce3f9b2dd109ea7b72
Moises Moraes <moisesmoraes7808@gmail.com>
2023-02-27 21:52:16 -0300

    first commit

---
+++ projeto1.java
@@ -0,0 +1,20 @@
+package br.com.dio.model;
+
+public class projeto1 {
+
+	public static void main(String[] args) {
+		
+		/*Criando variaveis */
+		
+		int a = 2;
+		int b = 4;
+		
+		System.out.println("Hello Mundo: " + (a+b));
+	
+		
+			
+		
+
+	}
+
+}
